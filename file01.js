73806c7b722174a704b01322d48c12aa2a11bfdb
siddharth <siddharth@siddharth.com>
2019-12-17 04:56:41 +0530

    another modification to file01.js

--- file01.js
+++ file01.js
@@ -2,7 +2,8 @@ function greet(name){
     console.log('knock knock knock ' + name )
     console.log('knock knock knock ' + name )
     console.log('knock knock knock ' + name )
-    console.log("Nice to have you here !")
+    console.log("Nice to have you here !") // this line has been staged i.e. added to the staging area
+    console.log("Would you like some tea ? ") // not staged as no git add command used after adding this line
 
 }
 
